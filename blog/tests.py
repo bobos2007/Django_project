8ff8ba391cfaa0a471b3519820697003a4fcd048
unknown <yongho1978@gmail.com>
2022-08-10 21:37:04 +0800

    작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from bs4 import BeautifulSoup
+from .models import Post
 
-# Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client=Client()
+        self.user_trump=User.object.create_user(
+
+            username='trump'
+            password='somepassword'
+
+
+        )
+
+post_001 = Post.object.create(
+
+    title='첫번째 포스트입니다.',
+    content='hell world',
+    author=self.user_trump
+
+
+
+)
+
+response=self.client.get('/blog/')
+soup=BeautifulSoup(response.content,'html.parser')
+self.assertIn(post_001,author.username.upper())
+
