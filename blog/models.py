765294c51d189a35ce78ea8e2b7c0da9a860b67e
unknown <yongho1978@gmail.com>
2022-08-13 19:05:48 +0800

    category model add

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+
+
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=100, unique=True)
+    slug=models.SlugField(max_length=100, unique=True, allow_unicode=True)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural='Categories'
+
+
 class Post(models.Model):
     title=models.CharField(max_length=50)
     hook_text=models.CharField(max_length=100,blank=True)
     content=models.TextField()
+
+
+
 
     head_image = models.ImageField(upload_to='blog/image/%Y/%m/%d', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
@@ -14,7 +29,8 @@ class Post(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
-    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    author= models.ForeignKey(User, on_delete=models.CASCADE)
+    category= models.ForeignKey(Category,blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}] {self.title} :: {self.author}'
