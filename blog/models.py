c10431e2a0b809341f2adc120729ecb83bc9e8c2
unknown <yongho1978@gmail.com>
2022-08-14 17:14:53 +0800

    category page만들기

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,10 @@ class Category(models.Model):
     slug=models.SlugField(max_length=100, unique=True, allow_unicode=True)
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
     class Meta:
         verbose_name_plural='Categories'
 
