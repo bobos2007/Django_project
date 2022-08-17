f3399fcd43bb594d9785861adfbc867f98ce9a7c
unknown <yongho1978@gmail.com>
2022-08-17 19:35:42 +0800

    tag기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,15 @@ class Category(models.Model):
         verbose_name_plural='Categories'
 
 
+class Tag(models.Model):
+    name=models.CharField(max_length=100, unique=True)
+    slug=models.SlugField(max_length=100, unique=True, allow_unicode=True)
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
 class Post(models.Model):
     title=models.CharField(max_length=50)
     hook_text=models.CharField(max_length=100,blank=True)
@@ -35,13 +44,14 @@ class Post(models.Model):
 
     author= models.ForeignKey(User, on_delete=models.CASCADE)
     category= models.ForeignKey(Category,blank=True, null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag ,blank=True)
 
     def __str__(self):
         return f'[{self.pk}] {self.title} :: {self.author}'
 
 
     def get_absolute_url(self):
-        return f'/blog/{self.pk}/'
+        return f'/blog/tag/{self.pk}/'
 
     def get_file_name(self):
         return os.path.basename(self.file_upload.name)
